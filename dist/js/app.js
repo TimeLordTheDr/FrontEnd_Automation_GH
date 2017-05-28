efcc211e8297940c6878a009099abdd5f696286f
Pavlo Lysak <repositoryUniversity@gmail.com>
2017-05-28 18:25:12 +0300

    Browserification clean-up

--- dist/js/app.js
+++ dist/js/app.js
@@ -4,7 +4,7 @@
 
 
 var uniq = require('uniq');
-var p = prompt('enter some sequence separated by comma. Browserification clean-up finished!!').split(',');
+var p = prompt('enter some sequence separated by comma. Browserification clean-up finished!!111222333000').split(',');
 console.log(uniq(p));
 
 //# sourceMappingURL=app.js.map
